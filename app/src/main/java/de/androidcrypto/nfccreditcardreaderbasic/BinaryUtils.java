711a23f0ba5e56c943949326d8ec03c34bd097b7
michaelfehr <michael_fehrgmx.de>
2023-03-11 01:41:35 +0100

    Initial commit

---
+++ app/src/main/java/de/androidcrypto/nfccreditcardreaderbasic/BinaryUtils.java
@@ -0,0 +1,233 @@
+package de.androidcrypto.nfccreditcardreaderbasic;
+
+import android.util.Base64;
+
+import java.math.BigInteger;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+
+public class BinaryUtils {
+
+    /**
+     * This class provides service methods for converting binary data
+     */
+
+    /**
+     * methods:
+     * String bytesToHex(byte[] bytes)
+     * byte[] hexToBytes(String str)
+     * String bytesToHexBlank(byte[] bytes)
+     * String formattedHexPrint(byte[] data, int address)
+     * String base64Encoding(byte[] input)
+     * byte[] base64Decoding(String input)
+     * String hexToBase64(String hexString)
+     * String base64ToHex(String base64String)
+     */
+
+    /**
+     * converts a byte array to a hex encoded string
+     * @param bytes
+     * @return hex encoded string
+     */
+    public static String bytesToHex(byte[] bytes) {
+        StringBuffer result = new StringBuffer();
+        for (byte b : bytes) result.append(Integer.toString((b & 0xff) + 0x100, 16).substring(1));
+        return result.toString();
+    }
+
+    /**
+     * converts a byte array to a decimal value string
+     * @param bytes
+     * @return a string with decimal values
+     */
+    public static String bytesToHexDecimal(byte[] bytes) {
+        return Arrays.toString(bytes);
+    }
+
+
+    /**
+     * converts a hex encoded string to a byte array
+     * @param str
+     * @return
+     */
+    public static byte[] hexToBytes(String str) {
+        byte[] bytes = new byte[str.length() / 2];
+        for (int i = 0; i < bytes.length; i++) {
+            bytes[i] = (byte) Integer.parseInt(str.substring(2 * i, 2 * i + 2),
+                    16);
+        }
+        return bytes;
+    }
+
+    public static byte[] hexBlankToBytes(String str) {
+        str = str.replaceAll(" ", "");
+        byte[] bytes = new byte[str.length() / 2];
+        for (int i = 0; i < bytes.length; i++) {
+            bytes[i] = (byte) Integer.parseInt(str.substring(2 * i, 2 * i + 2),
+                    16);
+        }
+        return bytes;
+    }
+
+    /**
+     * converts a byte array to a hex encoded string
+     * @param bytes
+     * @return hex encoded string with a blank after each value
+     */
+    public static String bytesToHexBlank(byte[] bytes) {
+        StringBuffer result = new StringBuffer();
+        for (byte b : bytes) result.append(Integer.toString((b & 0xff) + 0x100, 16).substring(1)).append(" ");
+        return result.toString();
+    }
+
+    /**
+     * converts a byte array to a formatted string with address, hex encoded values and ascii values
+     * @param data
+     * @param address int 0 as default, could be start address (offset)
+     * @return string like
+     */
+
+    public String formattedHexPrint(byte[] data, int address) {
+        // get hex address of part
+        String hexAddress = formatWithNullsLeft(Integer.toHexString(address), 8) + ":";
+        String hexContent = bytesToHexBlank(data);
+        // add blanks depending on data length (7 = add 3 blanks, 6 = add 6 blanks
+        for (int i = 0; i < (8 - data.length); i++) {
+            hexContent += "   ";
+        }
+        String asciiRowString = "";
+        for (int j = 0; j < data.length; j++) {
+            // check for maximal characters
+            asciiRowString = asciiRowString + returnPrintableChar(data[j], true);
+        }
+        String hexAscii = (char) 124 + formatWithBlanksRight(asciiRowString, 8);
+        return hexAddress + hexContent + hexAscii;
+    }
+
+    private static String formatWithNullsLeft(String value, int len) {
+        while (value.length() < len) {
+            value = "0" + value;
+        }
+        return value;
+    }
+
+    private static String formatWithBlanksRight(String value, int len) {
+        while (value.length() < len) {
+            value += " ";
+        }
+        return value;
+    }
+
+    private static char returnPrintableChar(byte inputByte, Boolean printDotBool) {
+        // ascii-chars from these ranges are printed
+        // 48 -  57 = 0-9
+        // 65 -  90 = A-Z
+        // 97 - 122 = a-z
+        // if printDotBool = true then print a dot "." for unprintable chars
+        char returnChar = 0;
+        if (printDotBool == true) {
+            returnChar = 46;
+        }
+        if ((inputByte >= 48) && (inputByte <= 57)) {
+            returnChar = (char) inputByte;
+        }
+        if ((inputByte >= 65) && (inputByte <= 90)) {
+            returnChar = (char) inputByte;
+        }
+        if ((inputByte >= 97) && (inputByte <= 122)) {
+            returnChar = (char) inputByte;
+        }
+        return returnChar;
+    }
+
+    /**
+     * converts a byte array to a Base64 encoded string
+     * @param input
+     * @return Base64 encoded string
+     */
+    public static String base64Encoding(byte[] input) {
+        return Base64.encodeToString(input, Base64.NO_WRAP);
+    }
+
+    /**
+     * converts a Base64 encoded string to a byte array
+     * @param input
+     * @return byte array
+     */
+    public static byte[] base64Decoding(String input) {
+        return Base64.decode(input, Base64.NO_WRAP);
+    }
+
+    /**
+     * converts a hex encoded string to a Base64 encoded string
+     * @param hexString
+     * @return Base64 encoded string
+     */
+    public static String hexToBase64(String hexString) {
+        return base64Encoding(hexToBytes(hexString));
+    }
+
+    /**
+     * converts a Base64 encoded string to hex encoded string
+     * @param base64String
+     * @return hex encoded string
+     */
+    public static String base64ToHex(String base64String) {
+        return bytesToHex(base64Decoding(base64String));
+    }
+
+    // int <-> byte array
+    // https://stackoverflow.com/a/7619315/8166854
+    public static byte[] intToByteArray(int value) {
+        return  ByteBuffer.allocate(4).putInt(value).array();
+    }
+
+    public static byte[] intToByteArrayV2(int value) {
+        return new byte[] {
+                (byte)(value >> 24),
+                (byte)(value >> 16),
+                (byte)(value >> 8),
+                (byte)value };
+    }
+
+    public static int intFromByteArray(byte[] bytes) {
+        return ByteBuffer.wrap(bytes).getInt();
+    }
+
+    // packing an array of 4 bytes to an int, big endian, minimal parentheses
+    // operator precedence: <<, &, |
+    // when operators of equal precedence (here bitwise OR) appear in the same expression, they are evaluated from left to right
+    public static int intFromByteArrayV2(byte[] bytes) {
+        return bytes[0] << 24 | (bytes[1] & 0xFF) << 16 | (bytes[2] & 0xFF) << 8 | (bytes[3] & 0xFF);
+    }
+
+    // packing an array of 4 bytes to an int, big endian, clean code
+    public static int intFromByteArrayV3(byte[] bytes) {
+        return ((bytes[0] & 0xFF) << 24) |
+                ((bytes[1] & 0xFF) << 16) |
+                ((bytes[2] & 0xFF) << 8 ) |
+                ((bytes[3] & 0xFF) << 0 );
+    }
+
+    // be careful when running in loops, calculation is costful
+    // https://stackoverflow.com/a/17981098/8166854
+    // this works for byte arrays of length 2
+    public static int intFromByteArrayV4(byte[] bytes) {
+        return new BigInteger(bytes).intValue();
+    }
+
+    public static byte[] intToByteArrayV4(int value) {
+        return new BigInteger(String.valueOf(value)).toByteArray();
+    }
+
+    public static int byteToInt(byte b) {
+        return (int) b & 0xFF;
+    }
+
+    public static int byteToInt(byte first, byte second) {
+        int value = (first & 0xFF) << 8;
+        value += second & 0xFF;
+        return value;
+    }
+
+}
---
+++ app/src/main/java/de/androidcrypto/nfccreditcardreaderbasic/nfccreditcards/PdolUtil.java
@@ -0,0 +1,155 @@
+package de.androidcrypto.nfccreditcardreaderbasic.nfccreditcards;
+
+import static de.androidcrypto.nfccreditcardreaderbasic.BinaryUtils.bytesToHex;
+import static de.androidcrypto.nfccreditcardreaderbasic.BinaryUtils.hexToBytes;
+
+import android.nfc.tech.IsoDep;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PdolUtil {
+
+    private final IsoDep nfc;
+    private final List<String> pdolList = new ArrayList<>();
+    private final int maximumPdolEntries = 20;
+    private boolean settingResult = setPdolEntries(maximumPdolEntries);
+    private final String pdolWithCountryCode =   "80A80000238321A0000000000000000001000000000008400000000000084007020300801733700000";
+    private final String pdolWithCountryCodeNew ="80A800002383B620C00000000000000001000000000000084000000000000840070203008017337000";
+    String c                                    ="80A80000158313C080000000000000100001240124823DDE7A0100";
+
+    // transaction amount much higher
+    private final String pdolWithCountryCode2 = "80A80000238321A0000000000010000001000000000008400000000000084007020300801733700000";
+    private final String pdolWithCountryCode3 = "80a800001283100000000000000000000000000000000000";
+/*
+Request :80 A8 00 00 12 83 10 B6 60 40 00 00 00 00 01 00 00 00 00 38 39 30 31 00
+Tag 9F 66: Terminal Transaction Qualifiers : B6 60 40 00
+Tag 9F 02: Transaction Amount :              00 00 00 01 00 00
+Tag 5F 2A: Transaction Currency Code :       03 56
+Tag 9F 37: Unpredictable Number :            38 39 30 31
+ */
+
+/*
+Commd Visa 9f66049f02069f03069f1a0295055f2a029a039c019f3704
+
+80A800002383 21A00000 000000000000 010000000000 0840 0000000000 0840 070203 00 80173370 0000
+                                              old                    working
+  9f66 04 Terminal Transaction Qualifiers : B6 60 40 00              A0 00 00 00
+  9f02 06 Transaction Amount :              00 00 00 01 00 00        00 00 00 00 00 01
+  9f03 06 Amount, Other (Numeric) always:   00 00 00 00 00 00
+  9f1a 02 Terminal Country Code :           08 26              UK
+  95   05 Terminal Verificat.Results alway: 00 00 00 00 00
+  5f2a 02 Transaction Currency Code :       08 26
+  9a   03 Transaction Date :                23 03 03
+  9c   01 Transaction Type :                00
+  9f37 04 Unpredictable Number :            38 39 30 31
+  Totl 33 (0x21)
+ */
+    private final String pdolVisaComdirect = "80A80000238321B6604000000000010000000000000000082600000000000826230303003839303100";
+    //private final String pdolVisaComdire = "80A80000238321A0000000000000000001000000000000084000000000000840070203008017337000";
+
+/*
+Lloyd Visa 9f66049f02069f03069f1a0295055f2a029a039c019f3704
+ */
+
+    public PdolUtil(IsoDep nfc) {
+        this.nfc = nfc;
+    }
+
+    /**
+     * getGpo means get the processing options
+     * @param preferredLength
+     * @return
+     */
+
+    public byte[] getGpo(int preferredLength) {
+        System.out.println("*** preferredLength: " + preferredLength);
+        String pdol;
+        if (preferredLength <= pdolList.size() && preferredLength > -1) {
+            //System.out.println("++ preferredLength ++");
+            // first test with the preferredLength, then test all
+            pdol = pdolList.get(preferredLength);
+            byte[] pdolCommand = hexToBytes(pdol);
+            System.out.println("***pdolCommand: " + bytesToHex(pdolCommand));
+            try {
+                byte[] pdolResult = nfc.transceive(pdolCommand);
+                System.out.println("***pdolResult: " + bytesToHex(pdolResult));
+                byte[] pdolResultOk = checkResponse(pdolResult);
+                if (pdolResultOk != null) return pdolResultOk;
+            } catch (IOException e) {
+                //throw new RuntimeException(e);
+                return null;
+            }
+        }
+        // at this point the preferredLength pdol was not working or the preferredLength was larger than the predefined lengths
+        // now loop through the predefined pdol's
+        //System.out.println("++ loop through predefinedPdol list with " + pdolList.size() + " entries ++");
+        for (int i = 0; i < pdolList.size(); i++) {
+            pdol = pdolList.get(i);
+            byte[] pdolCommand = hexToBytes(pdol);
+            System.out.println("++ pdolCommand: " + bytesToHex(pdolCommand));
+            try {
+                byte[] pdolResult = nfc.transceive(pdolCommand);
+                /*
+                if (pdolResult != null) {
+                    System.out.println("pdolResult: " + bytesToHex(pdolResult));
+                } else {
+                    System.out.println("pdolResult is NULL");
+                }
+                */
+                byte[] pdolResultOk = checkResponse(pdolResult);
+                if (pdolResultOk != null) return pdolResultOk;
+            } catch (IOException e) {
+                //throw new RuntimeException(e);
+                return null;
+            }
+        }
+        // at this point none of the predefined pdol is working
+        //System.out.println("++ predefinedPdols do not work, return null ++");
+        return null;
+    }
+
+    public String getPdolWithCountryCode() {
+        return pdolWithCountryCode;
+    }
+    public String getPdolVisaComdirect() {
+        return pdolVisaComdirect;
+    }
+    public String getPdolWithCountryCode2() {
+        return pdolWithCountryCode2;
+    }
+    private byte[] checkResponse(byte[] data) {
+        if (data.length < 5) return null; // not ok
+        int status = ((0xff & data[data.length - 2]) << 8) | (0xff & data[data.length - 1]);
+        if (status != 0x9000) {
+            return null;
+        } else {
+            return Arrays.copyOfRange(data, 0, data.length - 2);
+        }
+    }
+
+    private boolean setPdolEntries(int maximum) {
+        if (maximum > 100) return false;
+        // this is a sample string for a pdol entry with value 8:
+        // 80A800000a8308000000000000000000
+        for (int entry = 0; entry < maximum; entry ++) {
+            StringBuilder sb = new StringBuilder();
+            sb.append("80A80000");
+            sb.append(String.format("%02X", (entry + 2)));
+            sb.append("83");
+            sb.append(String.format("%02X", (entry)));
+            for (int i = 0; i < entry; i++) {
+                sb.append("00");
+            }
+            sb.append("00"); // trailing "00"
+            pdolList.add(sb.toString());
+        }
+        return true;
+    }
+    private String setPdolEntry(String pdolEntry) {
+        pdolList.add(pdolEntry);
+        return pdolEntry;
+    }
+}
